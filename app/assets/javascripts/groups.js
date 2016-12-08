557b07759c02ae096ab80fa1636c2e459459c9df
Vader0 <vader0@protonmail.com>
2016-12-08 15:10:44 +0100

    heroku shit

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,7 +1,7 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-//api-key: 9912e617f1d163b534e15c4c8b778da1
+// api_key = 9912e617f1d163b534e15c4c8b778da1
 function getEvents(){
   $.ajax({
       type: "GET",
@@ -12,14 +12,15 @@ function getEvents(){
 }
 
 function showEvents(response) {
-  console.log('Right path!')
+  var imageUrl  = 'https://image.tmdb.org/t/p/w500';
+  var posterUrl = response.poster_path;
+  var poster    = imageUrl + posterUrl;
+
+  $('#img').html('<img src="'+poster+'">');
+
+  console.log(response);
 }
 
 function handleError(error){
-  console.log('Higueputaa Malpario');
   console.log(error);
 }
-
-$(document).on('ready', function(){
-  getEvents();
-})
