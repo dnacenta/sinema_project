544e21a5050db5603ed58adec474896fa856a9ea
Vader0 <vader0@protonmail.com>
2016-12-09 11:17:51 +0100

    user profile root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get '/', to: 'site#home'
   get '/users/:user_id/groups/:id/add_user_to_group', to: 'groups#add_user_to_group'
   post '/users/:user_id/groups/:id/add_user_to_group', to: 'groups#add_user', as: :add_user
+  root 'users#show'
 
   resources :users do
     resources :groups
