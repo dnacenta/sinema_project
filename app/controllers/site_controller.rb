5134cc756f43c22ca7f37cdd8c088467b749c22e
Vader0 <vader0@protonmail.com>
2016-12-14 20:10:01 +0100

    about fixes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,10 @@
 class SiteController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def home
+    @user = current_user
+  end
+
   def about
     @user = current_user
   end
