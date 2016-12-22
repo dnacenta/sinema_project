fbe4c8306fad7d2e54b70b686e2c27b4d354cda1
Vader0 <vader0@protonmail.com>
2016-12-22 16:14:07 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create([
-  {name: 'Herve',     email: 'herve@herve.com', password: '1234567890'},
-  {name: 'Johnny',    email: 'johnny@johnny.com', password: '1234567890'},
-  {name: 'Mariano', email: 'mariano@mariano.com',    password: '1234567890'}
-])
-
-Group.create([
-  {name: 'Concesionario'},
-  {name: 'Francia Unida'},
-  {name: 'Mandingos'},
-  {name: 'Atos'},
-  {name: 'Tailandia'},
-  {name: 'Ladyboy'},
-  {name: 'Lemons'},
-  {name: 'Tirafrutas'},
-  {name: 'Familia Schwarz'}
-])
-
-u = User.find(1)
-u2 = User.find(2)
-u3 = User.find(3)
-
-u.groups.push(Group.find(1))
-u.groups.push(Group.find(2))
-u.groups.push(Group.find(3))
-
-u2.groups.push(Group.find(4))
-u2.groups.push(Group.find(5))
-u2.groups.push(Group.find(6))
-
-u3.groups.push(Group.find(7))
-u3.groups.push(Group.find(8))
-u3.groups.push(Group.find(9))
+# User.create([
+#   {name: 'Herve',     email: 'herve@herve.com', password: '1234567890'},
+#   {name: 'Johnny',    email: 'johnny@johnny.com', password: '1234567890'},
+#   {name: 'Mariano', email: 'mariano@mariano.com',    password: '1234567890'}
+# ])
+#
+# Group.create([
+#   {name: 'Concesionario'},
+#   {name: 'Francia Unida'},
+#   {name: 'Mandingos'},
+#   {name: 'Atos'},
+#   {name: 'Tailandia'},
+#   {name: 'Ladyboy'},
+#   {name: 'Lemons'},
+#   {name: 'Tirafrutas'},
+#   {name: 'Familia Schwarz'}
+# ])
+#
+# u = User.find(1)
+# u2 = User.find(2)
+# u3 = User.find(3)
+#
+# u.groups.push(Group.find(1))
+# u.groups.push(Group.find(2))
+# u.groups.push(Group.find(3))
+#
+# u2.groups.push(Group.find(4))
+# u2.groups.push(Group.find(5))
+# u2.groups.push(Group.find(6))
+#
+# u3.groups.push(Group.find(7))
+# u3.groups.push(Group.find(8))
+# u3.groups.push(Group.find(9))
